35a8a751de677c31bce9b149fe27e90397984df1
eniola4031 <agboola.eniola4031@gmail.com>
2021-08-12 02:14:17 +0100

    assignment 3

--- week6/tast3.sol
+++ week6/tast3.sol
@@ -1,19 +1,37 @@
 pragma solidity ^0.8.0;
 
 
-//A Street has 4 houses,each containing 6 flats that contains 5 rooms each.. 
+//A Street has 4 houses,each containing 3 flats that contains 5 rooms each.. 
 //Each room can hold any number of people....
 //Write a soldiity contract that calculates the total number of flats, 
 //rooms and occupants in that street
 contract arrayTask{
     
-    uint  [][5][6][4] public street= [
-    [[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]]],
-    [[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]]],
-    [[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]]],
-    [[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]],[[1,2],[1,2],[1,2],[1,2],[1,2]]]
+    uint  [][5][3][4] public street= [
+    [[[2],[1],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]]
+    ],
+    [[[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]]
+    ],
+    [[[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]]
+    ],
+    [[[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]],
+    [[2],[2],[2],[2],[2]]
+    ]
     ];
-        //uint [] public a = [1,2,3,4,5,6,7,8,9,10]; //dynamic array
-        //check for total flats
-uint x = street.length;
+    //access total houses on the street
+   uint public houses = street.length;
+      
+     //access total flats on the street
+     uint public flats= street.length*street[0].length;
+     //access total rooms
+     uint public rooms= flats*street[0][0].length;
+     //access total number of people in the rooms
+     uint public occupants= rooms*street[0][0][0].length;
 }
