e7df0820777720426c63d3a658e20b394264a997
eniola4031 <agboola.eniola4031@gmail.com>
2022-04-28 22:29:36 +0100

    mint xtra for lions

---
+++ xtra_for_lions/contracts/xtra.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/// ================= Imports ==================
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/// @title This is the LazyLion interface.
+ interface lazyLionI{
+    function balanceOf(address owner) external view returns (uint256 balance);
+ }
+
+//.-.  .-.   .-.   .-. .----. .-. .-. .----.
+// \ \/ /    | |   | |/  {}  \|  `| |{ {__  
+//  }  {     | `--.| |\      /| |\  |.-._} }
+// / /\ \     `----'`-' `----' `-' `-'`----'
+
+
+/// @title XTRA_for_LIONS
+/// @notice EXTRALIONS claimable by LazyLion owners
+/// @author Eniola Agboola
+/// @dev ERC721 claimable by members of a merkle tree
+contract XtraForLions is ERC721Enumerable, ReentrancyGuard, AccessControl {
+ using Counters for Counters.Counter;
+
+ Counters.Counter private _tokenIdCounter;
+
+     /// ==================== State Variables  =======================
+
+    bytes32 public constant MANAGER_ROLE = keccak256(abi.encodePacked("manager"));
+    bool public whitelistState;
+    address public admin;
+    uint256 public constant mintPrice = 60000000000000000; // 0.06 ether;
+    uint public maxMintKingAmount = 3;
+    uint public maxMintLazyAmount = 1;
+    uint public constant MaxTokenSupply = 500;
+    /// @notice whiteliste address inclusion root
+     bytes32 public  merkleRoot;
+     //this is a test lazyLion address
+    lazyLionI _lazyLion = lazyLionI(0x8FCD0A31f825FDb16D674279a2883Ea4ACfe6368);
+
+
+    /// ==================== mappings =======================
+             mapping(address => bool) public isWhitelisted;
+             mapping(address => uint) public _tokensMintedByAddress;
+
+
+
+    /// ==================== constructor =======================
+    /// @dev _merkleRoot must append "0x" prefix with the hash
+    /// @dev Grants `DEFAULT_ADMIN_ROLE` to the account that deploys the contract.
+    /// See {ERC20-constructor}.
+
+    constructor(bytes32 _merkleRoot, address _admin)ERC721("XTRA_for_LIONS","EXTRALIONS"){
+          admin = _admin;
+          merkleRoot = _merkleRoot; // Update root
+      _setRoleAdmin(MANAGER_ROLE, DEFAULT_ADMIN_ROLE);
+
+      _setupRole(DEFAULT_ADMIN_ROLE,_msgSender());// The creator of the contract is the default admin
+      _setupRole(DEFAULT_ADMIN_ROLE,admin);// We add a custom admin which could also be the gnosis instance     
+    }
+
+
+       /// ====================== events =======================
+      event UpdatedRoot(bytes32 _newRoot);
+      event managerAdded(address account);
+      event mintedLazyEdition(address addr, uint256 _mintAmount);
+      event mintedKingEdition(address addr, uint256 _mintAmount);
+
+
+    /// ====================== functions ========================
+
+     /// @notice Updates the merkleRoot with the given new root
+    /// @param _newRoot new merkleRoot to work with
+  function updateMerkleRoot(bytes32 _newRoot) onlyRole(MANAGER_ROLE) external {
+    merkleRoot = _newRoot;
+    emit UpdatedRoot(_newRoot);
+  }
+
+   /// @notice checks if the address is a member of the tree
+   /// @dev the proof and root are gotten from the MerkleTree script
+   /// @param _merkleProof to check if to is part of merkle tree
+    /// @notice Only whitelisted lazylion owners can mint
+   function mintLazyEdition(uint _mintAmount, bytes32[] calldata _merkleProof) nonReentrant external{
+     require(_lazyLion.balanceOf(msg.sender) > 0,"must be a lazy lion owner");
+  //generate a leaf node to verify merkle proof, or revert if not in tree
+     bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+    require(
+      MerkleProof.verify(_merkleProof,merkleRoot,leaf),
+      "invalid merkle proof");     //checks for valid proof
+
+                 isWhitelisted[msg.sender] = true;
+       require(_mintAmount != 0, "Cannot mint 0 tokens");     // Number of tokens can't be 0.
+       _tokensMintedByAddress[msg.sender] += _mintAmount; //update users record
+       require(
+         _tokensMintedByAddress[msg.sender] <= maxMintLazyAmount, 
+         "you have exceeded mint limit per wallet");        //check if address has minted
+        uint tokenLeft = totalSupply() + _mintAmount;
+        // Check that the number of tokens requested wouldn't exceed what's left.
+        require(tokenLeft <= MaxTokenSupply, "Minting would exceed max. supply");
+       uint256 mintIndex = totalSupply();
+        for(uint256 i = 0; i < _mintAmount; i++) {    // For each token requested, mint one.
+       if(mintIndex <= 120  && mintIndex <= MaxTokenSupply){
+             _safeMint(msg.sender, mintIndex);
+       }
+        }   
+        emit mintedLazyEdition(msg.sender, _mintAmount);
+   }
+
+    /// @notice only whitelisted address can mint if whitelist is disabled
+    /// @notice members can only mint this edition if they pay the mintPrice
+    /// @param _mintAmount is the min token amount
+    /// @param _merkleProof to make sure address is whitelisted
+   function mintKingEdition(uint _mintAmount, bytes32[] calldata _merkleProof) nonReentrant external payable{
+     //generate a leaf node to verify merkle proof, or revert if not in tree
+     bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+     //checks for valid proof
+    require(MerkleProof.verify(_merkleProof,merkleRoot,leaf),"invalid merkle proof");
+                 isWhitelisted[msg.sender] = true;
+        if(whitelistState == true){
+            require(
+            isWhitelisted[msg.sender] = true,"whitelist Enabled: only whiteListedAddress can mint"
+           ); 
+         }else{
+          (_lazyLion.balanceOf(msg.sender) > 0, "Whitelist disabled:not a lazy lion owner");     
+         }
+         // Number of tokens can't be 0.
+        require(_mintAmount != 0, "Cannot mint 0 tokens");
+      //  _tokensMintedByAddress[msg.sender][msg.sender]  += _mintAmount;//update users record
+             _tokensMintedByAddress[msg.sender]  += _mintAmount;//update users record
+
+        //check if address has minted
+        require(_tokensMintedByAddress[msg.sender] <= maxMintKingAmount, "you have exceeded mint limit per wallet");
+        uint tokenLeft = totalSupply() + _mintAmount;
+        // Check that the number of tokens requested wouldn't exceed what's left.
+        require(tokenLeft <= MaxTokenSupply, "Minting would exceed max. supply");
+        uint pricePerToken = mintPrice * _mintAmount;
+        // Check that the right amount of Ether was sent.
+        require(pricePerToken <= msg.value, "Not enough Ether sent."); 
+       uint256 mintIndex = totalSupply();
+               // For each token requested, mint one.
+        for(uint256 i = 0; i < _mintAmount; i++) {
+       if(mintIndex <= 380  && mintIndex <= MaxTokenSupply){
+             _safeMint(msg.sender, mintIndex);
+       }
+        }
+                emit mintedKingEdition(msg.sender, _mintAmount);
+
+   }
+
+
+      function changeWhitelistState() public onlyRole(MANAGER_ROLE){
+       whitelistState = !whitelistState;
+
+        }
+      function getBalance() onlyRole(MANAGER_ROLE) public view returns(uint256) {
+        return address(this).balance;
+        }
+
+       function changeLazyMintAmt(uint256 _newMint) public onlyRole(MANAGER_ROLE) {
+        maxMintLazyAmount = _newMint;
+        }
+       function changeKingMintAmt(uint256 _newMint) public onlyRole(MANAGER_ROLE) {
+        maxMintKingAmount = _newMint;
+         }
+// only role who have access to the gnosis safe can call this method and it will be deposit in gnosis for mulsig to sig
+         function withdraw() onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant external returns(bool){
+               (bool sent,) = payable(admin).call{value: getBalance()}("");
+            require(sent,"Ether not sent:failed transaction");
+                return true;
+         }
+    /// @dev Add an account to the manager role. Restricted to admins.
+        function addAsManager(address account) public onlyRole(DEFAULT_ADMIN_ROLE)
+       {
+         require(hasRole(MANAGER_ROLE,account) == false,"Already a manager");
+           grantRole(MANAGER_ROLE, account);
+
+            emit managerAdded(account);
+       }
+        
+      // Create a bool check to see if a account address has the role admin
+      function isAdmin(address account) public view returns(bool)
+      {
+           return hasRole(DEFAULT_ADMIN_ROLE, account);
+      }
+        // Create a bool check to see if a account address has the role admin
+      function isManager(address account) public view returns(bool)
+      {
+           return hasRole(MANAGER_ROLE, account);
+      }
+
+          ///@custom:interface for overridding the supportInterface method on AccessControl and ERC721Enumerable
+       function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+         ///@custom:interface for overridding the supportInterface method on AccessControl and ERC721Enumerable
+        function _beforeTokenTransfer(address from, address to,uint256 tokenId) internal virtual override(ERC721Enumerable) {
+                  super._beforeTokenTransfer(from, to, tokenId);
+
+        }
+
+
+}
---
+++ xtra_for_lions/scripts/mekleTree.js
@@ -0,0 +1,47 @@
+
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+
+// const { keccak256 } = require("ethers/lib/utils");
+
+
+let whitelist =  ["0x11056D6c881B5239A1b4A8bF5770D6C38C6b9131",
+ "0x4D3ABd536D0faD30E6475D43d108bD2d50397BfD",
+ "0xE3A9a11232f4D52786CA61f56bB7Fb01b00C80cd"]
+
+// The leaves, tree, and root are all PRE-DETERMINED prior to adding to whitelist
+
+//leaf node
+const leaves = whitelist.map(x => keccak256(x))
+//console.log("leaves value",leaves)
+const tree = new MerkleTree(leaves, keccak256,{sortPairs:true})
+//get the root hash of the markle tree in hex format
+const root = tree.getRoot().toString('hex')
+console.log ("whitelist Merkle Tree\n", tree.toString());
+console.log("Root Hash: ", root);
+
+const whitelistAddr = leaves[2];
+//console.log("leaves", whitelistAddr);
+const proof = tree.getHexProof(whitelistAddr)
+console.log("Merkle Proof:", proof);
+const verified = tree.verify(proof, whitelistAddr, root)
+
+console.log(verified) // true
+
+
+//["0x9dadd8333e9a0b1d875dae278dae592dba2169d08f1411cc3b5c3e88db14c38d"]
+
+// let whitelist =["0x617F2E2fD72FD9D5503197092aC168c91465E7f2",
+// "0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB",
+// "0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C",
+// "0xE3A9a11232f4D52786CA61f56bB7Fb01b00C80cd",
+// "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+// "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
+// "0x617F2E2fD72FD9D5503197092aC168c91465E7f2",
+// "0xdD870fA1b7C4700F2BD7f44238821C26f7392148",
+// "0x583031D1113aD414F02576BD6afaBfb302140225",
+// "0x11056D6c881B5239A1b4A8bF5770D6C38C6b9131",
+ //"0x11056D6c881B5239A1b4A8bF5770D6C38C6b9131",
+//"0x4D3ABd536D0faD30E6475D43d108bD2d50397BfD",
+//"0xE3A9a11232f4D52786CA61f56bB7Fb01b00C80cd"
+// ];
